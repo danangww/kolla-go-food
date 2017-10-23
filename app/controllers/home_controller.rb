412a547167e88a9bc87834015bd1c5f9e03ff4ef
danangww <danangww@yahoo.com>
2017-10-23 15:51:06 +0700

    add Home controller, User model, and user view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def hello
-    @time = Time.now
+    @time = Date.today
+    @users = User.all
+    @request = request
+    @response = response
+  end
+  def goodbye
+    @tomorrow = Date.today.next_day(1)
   end
 end
