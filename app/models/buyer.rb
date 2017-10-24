5e1a9ff079534d0a39ae9fdca990c93744b8164a
danangww <danangww@yahoo.com>
2017-10-24 16:47:26 +0700

    add validation to buyer

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,11 @@
 class Buyer < ApplicationRecord
+  validates :email, :name, :phone, :address, presence: true
+  validates :email, uniqueness:true, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+    message: 'email format is not valid.'
+  }
+  validates :phone, uniqueness: true, format: {
+    with: /\A[-+]?[0-9]*\.?[0-9]+\Z/,
+    message: 'phone format must be in number'
+  }, length: { maximum: 12 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :email, :name, :phone, :address, presence: true
+  validates :email, uniqueness:true
+  validates :email, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+    message: 'email format is not valid.'
+  }
+  validates :phone, format: {
+    with: /\A[-+]?[0-9]*\.?[0-9]+\Z/,
+    message: 'phone format must be in number'
+  }
+  validates :phone, length: { maximum: 12 }
 end
